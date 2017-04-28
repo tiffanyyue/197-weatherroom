e21c2304c73a9e9ec8282e71b1839846e492cdc6
Tiffany Yue <tyue@seas.upenn.edu>
2017-04-28 13:02:51 -0400

    XML to AJAX

--- js/weathercall.js
+++ js/weathercall.js
@@ -1,22 +1,32 @@
 weathercall = function(id) {
-	var xhr = new XMLHttpRequest();
 
-	var call = 'https://api.openweathermap.org/data/2.5/weather?id=' + id + '&APPID=1c5c83b160d3ca7ec76ccead0bd9d92d&units=imperial';
+	var call = 'http://api.openweathermap.org/data/2.5/weather?id=' + id + '&APPID=1c5c83b160d3ca7ec76ccead0bd9d92d&units=imperial';
 
-	xhr.open("GET", call, false);
+	$.ajax({
+	      type: 'GET',
+	      url: call,
+	      data: { 
+	        id: $('#data').text(),
+	        term: $('#searchBox').val() 
+	      },
+	      success: function(data) {
 
-	xhr.send();
-	var response = JSON.parse(xhr.responseText).weather[0].main;
+	        var response = data.weather[0].main;
 
-	if (response === "Clouds") {
-		triggerCloudy();
-	} else if (response === "Drizzle" || response === "Mist" || response === "Rain") {
-		triggerRain();
-	} else if (response === "Snow" || response === "Snowstorm" || response === "Flurry") {
-		triggerSnow();
-	} else if (response === "Clear") {
-		triggerSun();
-	}
-	updateTemp(JSON.parse(xhr.responseText).main.temp);
-	updateCondition(response);
+			if (response === "Clouds") {
+				triggerCloudy();
+			} else if (response === "Drizzle" || response === "Mist" || response === "Rain") {
+				triggerRain();
+			} else if (response === "Snow" || response === "Snowstorm" || response === "Flurry") {
+				triggerSnow();
+			} else if (response === "Clear") {
+				triggerSun();
+			}
+			updateTemp(data.main.temp);
+			updateCondition(response);
+	        
+	      }
+	    });
+
+
 }
